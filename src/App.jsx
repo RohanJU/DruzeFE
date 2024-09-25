04d04249c1e80ceae07de1ed3784738a0de8fd01
rohanju <rohanjadavpur@gmail.com>
2024-09-25 13:53:55 +0530

    final-code

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,15 @@ import Herosection from "./Components/Herosection";
 import Section1 from "./Components/Section1";
 
 function App() {
+  const handleScroll = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" }); // Smooth scroll
+    }
+  };
   return (
     <>
-      <Header />
+      <Header handleScroll={handleScroll} />
       <Herosection />
       <Section1 />
       <Clintfeedback />
